3282775a4eb0baa3ba1e1f1553726716c056e583
CXYhh121 <1637018959@qq.com>
2019-03-18 18:07:09 +0800

    牛客网专项练习代码

---
+++ 2019_3_18/2019_3_18/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int c;
+//	c = 13 | 5;
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//#define N 3+1
+//#define Y(n) ((N+1)*n)
+//
+//int main()
+//{
+//	int z = 2 * (N + Y(5 + 1));
+//	printf("%d\n", z);
+//	return 0;
+//}
+//
+//void foo(int *a, int *b)
+//{
+//	*a = *a + *b;
+//	*b = *a - *b;
+//	*a = *a - *b;
+//}
+//void main()
+//{
+//	int a = 1, b = 2, c = 3;
+//	foo(&a, &b);
+//	foo(&b, &c);
+//	foo(&c, &a);
+//	printf("%d, %d, %d", a, b, c);
+//}
+
+//
+//int main()
+//{
+//	int i = -2147483648;
+//	return printf("%d,%d,%d,%d", ~i, -i, 1 - i, -1 - i);
+//}
---
+++ 2019_3_18/2019_3_18/test.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+//class A
+//{
+//public:
+//	void f()
+//	{
+//		printf("A\n");
+//	}
+//};
+//
+//class B : public A
+//{
+//public:
+//	virtual void f()
+//	{
+//		printf("B\n");
+//	}
+//};
+//
+//int main()
+//{
+//	A *a = new B;
+//	a->f();
+//	delete a;
+//	return 0;
+//}
