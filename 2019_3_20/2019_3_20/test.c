c120080f7ed6b37428e46aa567709167ca290374
CXYhh121 <1637018959@qq.com>
2019-03-20 18:55:35 +0800

    《C与指针》学习代码，以及牛客网专项练习

---
+++ 2019_3_20/2019_3_20/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int foo(int n){
+//	if (n<2)
+//	{
+//		return n;
+//	}
+//	else
+//		return 2 * foo(n - 1) + foo(n - 2);
+//}
+//
+//int main()
+//{
+//	int ret = foo(5);
+//
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char str[] = "ab\012\\n";
+//	printf("%d", strlen(str));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+//	char a[] = "abcdef";
+//	a[0] = 'b';
+//	a[1] = 'r';
+//	return 0;
+//}
+//int main()
+//{
+//	char a[] = "abcdef";
+//	a[] = "ghjk";
+//	return 0;
+//}
+
+//typedefºÍ#define
+
+//#include <stdio.h>
+//
+//#define d_ptr_to_char  char*
+//typedef char*  ptr_to_char;
+//
+//int main()
+//{
+//
+//	ptr_to_char a, b;
+//	d_ptr_to_char c, d;
+//
+//	b = "abcj";
+//	d = "hgjfgj";
+//	return 0;
+//}
