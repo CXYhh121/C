dcf4c26f1ad629393d074e8d949e234fbeabcda4
CXYhh121 <1637018959@qq.com>
2019-03-24 11:45:27 +0800

    牛客网专项练习代码

---
+++ 2019_3_24/2019_3_24/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int x;
+//	float y;
+//	scanf("%3d%f", &x, &y);
+//
+//	printf("%3d %f", x, y);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char array[] = "abcdefg";
+//	printf("%d\n", sizeof(array));
+//	char *p = "abcdefg";
+//	printf("%d\n", sizeof(p));
+//	return 0;
+//}
+//
+//void func1(char p[10])
+//{
+//	printf("%d\n", sizeof(p));
+//}
+//void func2(char(& p)[10])
+//{
+//	printf("%d\n", sizeof(p));
+//
+//}
+//int main(void)
+//{
+//	printf("%d\n", sizeof(char[2]));
+//	printf("%d\n", sizeof(char &));
+//	return 0;
+//}
