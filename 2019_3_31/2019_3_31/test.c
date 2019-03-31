7734e5fecbf72c7029a4e746ab4f4c18849cbaa5
CXYhh121 <1637018959@qq.com>
2019-03-31 18:22:31 +0800

    牛客网专项练习代码

---
+++ 2019_3_31/2019_3_31/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+//void GetMemeory(char* p)
+//{
+//	p = (char*)malloc(100);
+//}
+//
+//void test()
+//{
+//	char *str = NULL;
+//	GetMemeory(str);
+//	strcpy(str, "Thunder");
+//	strcat(str + 2, "Downloader");
+//	printf(str);
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int a = 2, b = -1, c = 2;
+//	if (a < b)
+//	{
+//		if (b < 0) c = 0;
+//		else c++;
+//	}  printf("%d\n", c);
+//	return 0;
+//}
+
---
+++ 2019_3_31/2019_3_31/test.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+//计算一个结构体的大小
+//struct xx
+//{
+//	long long _x1;
+//	char _x2;
+//	int _x3;
+//	char _x4[2];
+//	static int _x5;
+//};
+//int xx::_x5;
+//
+//int main()
+//{
+//	cout << sizeof(xx) << endl;
+//	return 0;
+//}
