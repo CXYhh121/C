ab70fc5904643f39a812fa3bf955a519cdf7e640
CXYhh121 <1637018959@qq.com>
2019-04-04 16:40:44 +0800

    牛客网专项练习代码

---
+++ 2019_4_4/2019_4_4/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//main()
+//{
+//	static char a[3];
+//	scanf("%s", a);
+//	printf("%c,%c", a[1], a[2]);
+//}
+
+//#include <stdio.h>
+//int main() {
+//	char str[] = "ABCD", *p = str;
+//	printf("%d\n", *(p + 4));
+//	return 0;
+//};
---
+++ 2019_4_4/2019_4_4/test.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+
+//int main()
+//{
+//	int a[2][5] = { { 2, 3, 4, 5, 6 }, { 7, 8, 9, 10, 11 } };
+//	int *ptr = (int*)(&a + 1);
+//	cout << *(ptr - 3) << endl;
+//
+//	return 0;
+//}
+
+//class A
+//{
+//public:
+//	A(){ cout<<"A"; }
+//	~A(){
+//		printf("~A");
+//	}
+//};
+//class B : public A
+//{
+//public:
+//	B(){ printf("B"); }
+//	~B(){ printf("~B"); }
+//};
+//
+//int main()
+//{
+//	A* c = new B[2];
+//	delete[] c;
+//	return 0;
+//}
