7a76db5b41b73d73d6ee29c5b00bb805c7b82bfe
CXYhh121 <1637018959@qq.com>
2019-05-04 07:20:32 +0800

    牛客网专项练习代码

---
+++ 2019_5_4/2019_5_4/test.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+//int fun(int* s, int n)
+//{
+//	int i, k;
+//	for (i = 0, k = i; i < n; i++)
+//	if (s[i] > s[k])
+//		k = i;
+//	return (k);
+//}
+//int main()
+//{
+//	int s[] = { 4, 2, 5, 8, 1, 6, 9 };
+//	cout << fun(s, sizeof(s)/ sizeof(s[0]));
+//	return 0;
+//}
+//
+//#define ADD(x)  x*x
+//main()
+//{
+//	//4+6*4+6*7 = 4+24+42=70
+//	int a = 4, b = 6, c = 7, d = ADD(a + b)*c;
+//	printf("d=%d", d);
+//}
+
+//a3_     a3_   a_3   3_a   3a_   _3a   _a3
+
+//int f(int x) {
+//	cout << "1" << endl;
+//	if (x <= 2)
+//		return 1;
+//	return f(x - 2) + f(x - 4) + 1;
+//}
+//
+//int main()
+//{
+//	f(10);
+//	//cout << f(10) << endl;
+//	return 0;
+//}
